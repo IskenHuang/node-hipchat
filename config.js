1c6a7f6289dca30be096f67f22831d8ec3f18f81
Isken Huang <kensheep@gmail.com>
2013-10-08 14:16:32 +0800

    add config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ * HipChat
+ *
+ * HipChat API Doc
+ * https://www.hipchat.com/docs/api
+ *
+ * @module      :: Service
+ * @description :: hipchat config
+ */
+module.exports = {
+
+    token: 'YOUR_TOKEN',
+
+    roomId: 'YOUR_ROOM_ID'
+};
